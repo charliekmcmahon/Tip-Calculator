c1873305b21bf6736710bed8230396fe3ab602b0
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-02-24 11:19:37 +1000

    update

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,15 +1,13 @@
-//Calculate Tip
 function calculateTip() {
   var billAmt = document.getElementById("billamt").value;
   var serviceQual = document.getElementById("serviceQual").value;
   var numOfPeople = document.getElementById("peopleamt").value;
 
-  //validate input
   if (billAmt === "" || serviceQual == 0) {
     alert("Please enter values");
     return;
   }
-  //Check to see if this input is empty or less than or equal to 1
+
   if (numOfPeople === "" || numOfPeople <= 1) {
     numOfPeople = 1;
     document.getElementById("each").style.display = "none";
@@ -17,21 +15,34 @@ function calculateTip() {
     document.getElementById("each").style.display = "block";
   }
 
-  //Calculate tip
+
   var total = (billAmt * serviceQual) / numOfPeople;
-  //round to two decimal places
+  var totalmeal = (billAmt / numOfPeople);
+
   total = Math.round(total * 100) / 100;
-  //next line allows us to always have two digits after decimal point
   total = total.toFixed(2);
-  //Display the tip
+  total = parseInt(total);
+
+  totalmeal = Math.round(totalmeal * 100) / 100;
+  totalmeal = totalmeal.toFixed(2);
+  totalmeal = parseInt(totalmeal);
+
+
+  var totalCostOfMealWithTip = totalmeal + total;
+
+
   document.getElementById("totalTip").style.display = "block";
+  document.getElementById("mealCost").style.display = "block";
   document.getElementById("tip").innerHTML = total;
+  document.getElementById("cost").innerHTML = totalCostOfMealWithTip;
 
 }
 
 //Hide the tip amount on load
 document.getElementById("totalTip").style.display = "none";
+document.getElementById("mealCost").style.display = "none";
 document.getElementById("each").style.display = "none";
+document.getElementById("each2").style.display = "none";
 
 //click to call function
 document.getElementById("calculate").onclick = function() {
